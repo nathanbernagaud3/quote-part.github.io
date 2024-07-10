ab8e09c98da23680bc8e3eb8af5757201477f49d
nathanbernagaud3 <nathanbernagaud3@gmail.com>
2024-07-10 22:47:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function calculer() {
     // Obtenir les valeurs des entrées
-    const bienImmo = parseFloat(document.getElementById('bienImmo').value);
     const conjointSurvivant = document.getElementById('conjointSurvivant').value;
     const nombreEnfants = parseInt(document.getElementById('nombreEnfants').value);
     
     let result;
 
-    // Exemple de conversion de la formule C12
+    // Exemple de conversion de la formule C12 sans la valeur du bien immobilier
     if (conjointSurvivant === "100% Usufruit") {
-        result = 0;
+        result = "0%";
     } else if (conjointSurvivant === "100% Pleine Propriété") {
-        result = bienImmo;
+        result = "100%";
     } else if (conjointSurvivant === "1/4 Usufruit") {
-        result = 0;
+        result = "0%";
     } else if (conjointSurvivant === "1/4 Pleine Propriété") {
-        result = (1/4) * bienImmo;
+        result = "25%";
     } else if (conjointSurvivant === "1/4 Pleine Propriété et 3/4 Usufruit") {
-        result = (1/4) * bienImmo;
+        result = "25%";
     } else if (conjointSurvivant === "Pas de Conjoint Survivant") {
-        result = 0;
+        result = "0%";
     } else {
-        // Ajoutez les autres conditions ici
+        result = "Erreur : Option non reconnue";
     }
 
     // Afficher le résultat
